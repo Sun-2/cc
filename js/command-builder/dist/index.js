4bc24d289bf7b78e575698b986bb85b429ed4081
Sun-2 <sun127.0.0.1@gmail.com>
2020-11-22 23:42:32 +0100

    remove dist

--- js/command-builder/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHello = void 0;
-var sayHello = function () { return console.log('hello'); };
-exports.sayHello = sayHello;
-//# sourceMappingURL=index.js.map
